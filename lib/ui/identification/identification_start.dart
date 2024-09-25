e80e303994e1dfc11bf0e7ec1b649369b52764dc
Nick Warriner <nwarriner@magicwrighter.com>
2024-09-25 18:20:26 -0400

    Tweak single id UI

--- lib/ui/identification/identification_start.dart
+++ lib/ui/identification/identification_start.dart
@@ -13,12 +13,12 @@ class IdentifyMedicationStart extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(8.0),
           child: IntrinsicHeight(
             child: IntrinsicWidth(
               child: Card(
                 child: Padding(
-                  padding: const EdgeInsets.all(16.0),
+                  padding: const EdgeInsets.all(8.0),
                   child: Column(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
